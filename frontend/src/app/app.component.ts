70d43cee5fda7c16c45fb4358f17868c15ec2347
-27IQ <83514344+27IQ@users.noreply.github.com>
2026-02-02 02:44:06 +0100

    schwänz

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sidebar2Component } from './base-components/sidebar-2/sidebar-2.component';
-import { SidebarComponent } from './base-components/sidebar/sidebar.component';
-import { PageTitleComponent } from './base-components/page-title/page-title.component';
-import { Sidebar2TitleComponent } from './base-components/sidebar-2-title/sidebar-2-title.component';
-import { HomeIconComponent } from './base-components/home-icon/home-icon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar2Component, SidebarComponent, PageTitleComponent, Sidebar2TitleComponent, HomeIconComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Sidebar2Component } from '../base-components/sidebar-2/sidebar-2.component';
+import { SidebarComponent } from '../base-components/sidebar/sidebar.component';
+import { PageTitleComponent } from '../base-components/page-title/page-title.component';
+import { Sidebar2TitleComponent } from '../base-components/sidebar-2-title/sidebar-2-title.component';
+import { HomeIconComponent } from '../base-components/home-icon/home-icon.component';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [Sidebar2Component, SidebarComponent, PageTitleComponent, Sidebar2TitleComponent, HomeIconComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
